f199df160f6717a4b69a0be4f9cd32654eca99fc
231165-diseño <231165@unamba.edu.pe>
2026-01-13 19:05:17 -0500

    Proyecto inicial: Catálogo de productos Spring Boot

---
+++ src/main/java/com/catalogo/productcatalog/ProductCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.catalogo.productcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoductCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoductCatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/catalogo/productcatalog/RoductCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catalogo.productcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoductCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
